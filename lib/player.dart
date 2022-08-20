262e213d23744d1942b43ff347137be053fd185f
Sara Liljefors <sara_liljefors@hotmail.com>
2022-08-20 19:11:30 +0200

    ny katt-animation

--- lib/player.dart
+++ lib/player.dart
@@ -9,7 +9,7 @@ class Player extends SpriteAnimationGroupComponent<PlayerState> {
   Future<void> onLoad() async {
     size = Vector2(20, 20);
     add(RectangleHitbox());
-    current = PlayerState.down;
+    current = PlayerState.side;
     await Flame.images.loadAll([
       "katt1.png",
       "katt2.png",
@@ -20,15 +20,19 @@ class Player extends SpriteAnimationGroupComponent<PlayerState> {
       "kattfram1.png",
       "kattfram2.png",
       "kattfram3.png",
+      "nykatt1.png",
+      "nykatt2.png",
+      "nykatt3.png",
     ]);
     animations = {
       PlayerState.side: SpriteAnimation.spriteList(
         [
-          Sprite(Flame.images.fromCache("katt1.png")),
-          Sprite(Flame.images.fromCache("katt2.png")),
-          Sprite(Flame.images.fromCache("katt3.png")),
+          Sprite(Flame.images.fromCache("nykatt1.png")),
+          Sprite(Flame.images.fromCache("nykatt2.png")),
+          Sprite(Flame.images.fromCache("nykatt1.png")),
+          Sprite(Flame.images.fromCache("nykatt3.png")),
         ],
-        stepTime: 0.2,
+        stepTime: 0.6,
       ),
       PlayerState.idle: SpriteAnimation.spriteList(
         [
