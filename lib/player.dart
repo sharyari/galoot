61a2a31b4bfd11f10a9100a98e93633a7ce97492
Sara Liljefors <sara_liljefors@hotmail.com>
2022-08-20 14:32:55 +0200

    bättre katt-animation

--- lib/player.dart
+++ lib/player.dart
@@ -7,26 +7,35 @@ class Player extends SpriteAnimationGroupComponent<PlayerState> {
   @override
   Future<void> onLoad() async {
     current = PlayerState.up;
+    await Flame.images.loadAll([
+      "katt1.png",
+      "katt2.png",
+      "katt3.png",
+      "kattbak1.png",
+      "kattbak2.png",
+      "kattbak3.png",
+    ]);
     animations = {
       PlayerState.side: SpriteAnimation.spriteList(
         [
-          Sprite(await Flame.images.load("katt1.png")),
-          Sprite(await Flame.images.load("katt2.png")),
-          Sprite(await Flame.images.load("katt3.png")),
+          Sprite(Flame.images.fromCache("katt1.png")),
+          Sprite(Flame.images.fromCache("katt2.png")),
+          Sprite(Flame.images.fromCache("katt3.png")),
         ],
         stepTime: 0.2,
       ),
       PlayerState.idle: SpriteAnimation.spriteList(
         [
-          Sprite(await Flame.images.load("katt1.png")),
+          Sprite(Flame.images.fromCache("katt1.png")),
         ],
         stepTime: 0.2,
       ),
       PlayerState.up: SpriteAnimation.spriteList(
         [
-          Sprite(await Flame.images.load("kattbak1.png")),
-          Sprite(await Flame.images.load("kattbak2.png")),
-          Sprite(await Flame.images.load("kattbak3.png")),
+          Sprite(Flame.images.fromCache("kattbak1.png")),
+          Sprite(Flame.images.fromCache("kattbak2.png")),
+          Sprite(Flame.images.fromCache("kattbak1.png")),
+          Sprite(Flame.images.fromCache("kattbak3.png")),
         ],
         stepTime: 0.2,
       ),
