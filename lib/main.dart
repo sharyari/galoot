4ac3b1a12c721edb42aa68d5e8ab3a4de850f84e
jonathan <sharyari@gmail.com>
2022-08-20 14:31:14 +0200

    add world and movement

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:galoot/assets.dart';
+import 'package:galoot/level.dart';
+import 'package:galoot/player.dart';
 
 void main() {
   runApp(GameWidget(game: GalootGame()));
 }
 
-class GalootGame extends FlameGame {
+Future<JoystickComponent> getJoystick() async {
+  return JoystickComponent(
+      knob: SpriteComponent(
+        sprite: Sprite(await Flame.images.load('knob.png')),
+        size: Vector2.all(70),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(await Flame.images.load('knob_bg.png')),
+        size: Vector2.all(150),
+      ),
+      margin: const EdgeInsets.only(left: 40, bottom: 40));
+}
+
+class GalootGame extends FlameGame with HasCollisionDetection, HasDraggables {
+  late JoystickComponent joystick;
   @override
   Future<void> onLoad() async {
-    add(
-      CircleComponent(position: Vector2(100, 100), radius: 5),
-    );
+    debugMode = true;
+    camera.zoom = 2.0;
+    joystick = await getJoystick();
+    add(Player(joystick));
+    add(joystick);
+    add(Level(world, collection));
   }
 }
--- lib/player.dart
+++ lib/player.dart
@@ -1,11 +1,14 @@
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
+import 'package:flame/collisions.dart';
 
 class Player extends SpriteAnimationGroupComponent<PlayerState> {
-  Player() : super(size: Vector2(200, 200));
-
+  final JoystickComponent joystick;
+  Player(this.joystick) : super(size: Vector2(200, 200));
   @override
   Future<void> onLoad() async {
+    size = Vector2(20, 20);
+    add(RectangleHitbox());
     current = PlayerState.up;
     animations = {
       PlayerState.side: SpriteAnimation.spriteList(
@@ -31,6 +34,14 @@ class Player extends SpriteAnimationGroupComponent<PlayerState> {
         stepTime: 0.2,
       ),
     };
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (!joystick.delta.isZero()) {
+      position.add(joystick.delta);
+    }
   }
 }
 
